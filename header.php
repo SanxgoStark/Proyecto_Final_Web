df9286a4d1e34b098155424a43716e8b3437572b
SanxgoStark <17030203@itcelaya.edu.mx>
2021-01-16 23:06:56 -0600

    modificacion de header e index

--- header.php
+++ header.php
@@ -1,11 +1,9 @@
 <?php
 
 ?>
+<div style="max-width: 100%">
 
-<!-- Inicio de carrusel -->
-<div style="position: absolute;width: 100%;height: 100%; align-content: center;">
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <a style="font-family: sans-serif;font-size: 25px" class="navbar-brand" href="">PexSales</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -14,21 +12,21 @@
   <div class="collapse navbar-collapse" id="navbarColor01">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home
+        <a class="nav-link" href="#">
           <span class="sr-only">(current)</span>
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="registro.php">Registro</a>
+        <a class="nav-link" href="#"></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="login.php">Loggin</a>
+        <a class="nav-link" href="#"></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="contraseña.php">Contraseña</a>
+        <a class="nav-link" href="#"></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="about.html">About</a>
+        <a class="nav-link" href="#"></a>
       
     </ul>
     
@@ -40,6 +38,4 @@
   </button>
 
 </nav>
-
-
 </div>
--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
   session_start();
   session_destroy();
+
+
+  include "header.php"; // cabecera con enlaces hacia css
+
 ?>
-<!DOCTYPE html>
+
 <html>
 <head>
 	<title>Pexsales</title>
@@ -21,48 +25,12 @@
     </div>
   </div>
 </div>  -->
-<div style="position: absolute;width: 100%;height: 100%; align-content: center;">
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a style="font-family: sans-serif;font-size: 25px" class="navbar-brand" href="">PexSales</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarColor01">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">
-          <span class="sr-only">(current)</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#"></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#"></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#"></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#"></a>
-      
-    </ul>
-    
-  </div>
 
-  <button type="button" class="btn btn-success">
-      <img src="img/login.png"/>
-      <a style="color:white" href="loginn.php">Login</a>
-  </button>
 
-</nav>
 
-<div class="container">
-  <div class="row renglon">
-    <div class="col-md-12">
-
+<!-- <div style="">
 <section class="carousel" aria-label="Gallery">
   <ol class="carousel__viewport">
     <li id="carousel__slide1"
@@ -125,11 +93,7 @@
     </ol>
   </aside>
 </section>
-
-</div>
-  </div>
-    </div>
-</div>
+</div> -->
 </body>
 </html>
 
